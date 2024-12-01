1aed8d9f50620ba48ceeb0853cc8a5e50f6f60e2
bune <bune_assassin@hotmail.com>
2024-11-30 22:47:13 -0600

    imagen

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class ImagenController extends Controller
 {
@@ -10,39 +11,39 @@ class ImagenController extends Controller
     {
         // Validar la imagen
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // 2MB máximo
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Máximo 2MB
         ]);
 
-        // sacamos la imagen del request
+        // Obtener el archivo de la imagen del request
         $image = $request->file('image');
 
-        $imagePath = $image->store('images', 'public');
+        // Subir la imagen a Cloudinary
+        $uploadedImage = Cloudinary::upload($image->getRealPath(), [
+            'folder' => 'images', // Opcional: carpeta en Cloudinary
+        ]);
 
-        // Guardar la ruta de la imagen en la base de datos
+        // Obtener la URL segura de la imagen subida
+        $imageUrl = $uploadedImage->getSecurePath();
+
+        // Guardar la URL de la imagen en la base de datos
         $user = $request->user();
-        $user->foto_perfil = $imagePath;
+        $user->foto_perfil = $imageUrl;
         $user->save();
 
-        return response()->json(['message' => 'Imagen guardada correctamente.'], 200);
+        return response()->json(['message' => 'Imagen guardada correctamente.', 'url' => $imageUrl], 200);
     }
+
     public function ver(Request $request)
     {
         $user = $request->user();
-        $fotodefault = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
-        // Verificar si el foto de perfil es la defecto
-        if ($user->foto_perfil === $fotodefault) {
-            return response()->json([
-                'user' => [
-                    'foto_perfil' => $fotodefault,
-                ]
-            ], 200);
-        }
-        // Asegúrate de que $user->foto_perfil tenga solo el nombre del archivo, sin rutas adicionales
-        $imageUrl = url('storage/images/' . basename($user->foto_perfil));
+        $fotoDefault = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y';
+
+        // Verificar si el usuario tiene una foto de perfil
+        $fotoPerfil = $user->foto_perfil ?: $fotoDefault;
 
         return response()->json([
             'user' => [
-                'foto_perfil' => $imageUrl,
+                'foto_perfil' => $fotoPerfil,
             ]
         ], 200);
     }
